aad04211c7f078262b965c0254576d3b2fa5c3df
Panagiotis Adamopoulos <padamop@gmail.com>
2012-09-28 18:34:42 -0300

    Update PreliminaryProgrammingExercises/IntroductionToDictionaries.py
    v1.10 - Adding documentation to the preliminary programming exercises. 

--- PreliminaryProgrammingExercises/IntroductionToDictionaries.py
+++ PreliminaryProgrammingExercises/IntroductionToDictionaries.py
@@ -6,20 +6,23 @@ Objectives:
 '''
 
 # Create a dictionary
-given = ['John', 'Eric', 'Terry', 'Michael']
+given = ['John', 'Eric', 'Terry', 'Michael'] # simple python list
 family = ['Cleese', 'Idle', 'Gilliam', 'Palin']
 
 # How  to build a dictionary from two lists (or sequences): one list of keys, another list of values
+# In this example keys:= given and corresponding values:= family
 names = dict(zip(given, family))
 
-print('Dictionary: '), # You can also print on the same line
-print(names)
+# The next lines of code will help you understand even better the dictionary that we created
+print('Dictionary: '), # Tip: You can also use 'print' to print on the same line
+print(names) # print the entire dictionary
 print('Dictionary keys: '), 
-print(names.keys())
+print(names.keys()) # just the dictionary keys
 print('Dictionary values: '),
-print(names.values())
+print(names.values()) # just the dictionary values
 print('')
 
 print('You can also write a simple loop..')
+# iteritems(), iterkeys(), and itervalues() provide you with useful iterators over the dictionary’s (key, value) pairs, keys, and values, respectively.
 for (given, family) in names.iteritems():
-    print given, family
+    print given, family # of course, we would get the result with print given, names[given]
